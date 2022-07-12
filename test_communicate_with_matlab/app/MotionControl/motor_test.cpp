d863bff49dca08443628f0e264343d10d71695b8
AuroraDeng <89623368+AuroraDeng@users.noreply.github.com>
2022-07-12 15:05:44 +0800

    Update motor_test.cpp

--- test_communicate_with_matlab/app/MotionControl/motor_test.cpp
+++ test_communicate_with_matlab/app/MotionControl/motor_test.cpp
@@ -9,8 +9,6 @@ void test(int32_t target_position)
 	if(!motor_test.IsHomePosition())
 		motor_test.BackToHomePosition();
 	
-	delay_ms(10);
-	
 	println_str(&UART1_Handler,"The motor is at home position.");	
 	motor_test.MoveToPosition(3000,motor_test.ProfileAcc,motor_test.ProfileDec,1,target_position);
 }
